521626e40099f4ced9fd8d239068e3ecbfd61622
Henrik Bulskov <bulskov@ruc.dk>
2022-11-05 23:27:07 +0100

    Add project files.

---
+++ DataLayer/User.cs
@@ -0,0 +1,11 @@
+﻿namespace DataLayer
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Username { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+        public string Salt { get; set; } = String.Empty;
+    }
+}
---
+++ WebServiceHardcodedUser/Controllers/MoviesController.cs
@@ -0,0 +1,65 @@
+﻿using DataLayer;
+using Microsoft.AspNetCore.Mvc;
+using WebServiceHardcodedUser.Models;
+
+namespace WebServiceHardcodedUser.Controllers
+{
+    [ApiController]
+    [Route("api/v1/movies")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IDataService _dataService;
+        private readonly LinkGenerator _generator;
+
+        public MoviesController(IDataService dataService, LinkGenerator generator)
+        {
+            _dataService = dataService;
+            _generator = generator;
+        }
+
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            try
+            {
+                var movies = _dataService.GetMovies(-1);
+                return Ok(movies.Select(CreateMovieDto));
+            }
+            catch 
+            {
+                return Unauthorized();
+            }
+        }
+
+        [HttpGet("{movieId}", Name = nameof(GetMovie))]
+        public IActionResult GetMovie(string movieId)
+        {
+            try
+            {
+                var movie = _dataService.GetMovie(-1, movieId);
+                return Ok(CreateMovieDto(movie));
+            }
+            catch
+            {
+                return Unauthorized();
+            }
+        }
+
+        /**
+         *
+         * Helper
+         *
+         */
+
+        
+        private MovieModel CreateMovieDto(Movie? movie)
+        {
+            return new MovieModel
+            {
+                Url = _generator.GetUriByName(HttpContext, nameof(GetMovie), new { movieId = movie?.Id}),
+                Title = movie?.Title,
+                Type = movie?.Type
+            };
+        }
+    }
+}
